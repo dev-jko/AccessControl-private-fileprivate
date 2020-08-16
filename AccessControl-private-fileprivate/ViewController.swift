a42184b947f2785bc216e088496f8c012169a0fc
nadarm <ccc7497@gmail.com>
2020-08-16 17:06:46 +0900

    Initial Commit

---
+++ AccessControl-private-fileprivate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccessControl-private-fileprivate
+//
+//  Created by Jaedoo Ko on 2020/08/16.
+//  Copyright © 2020 jko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
